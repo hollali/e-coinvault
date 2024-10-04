64c7b8b601c6d4da040e1f39702d3e0ee92bd872
hollali <dheztinykartel@gmail.com>
2024-10-04 18:04:49 +0000

    "Updated Header component with new styles and added navigation items"

--- components/header.js
+++ components/header.js
@@ -1,30 +1,48 @@
-import React from 'react'
-import Image from 'next/image'
-
+import React from "react";
+import Image from "next/image";
 
 const styles = {
-  navLink: `text-white flex mx-[10px]`,
-  badge: `rounded-full bg-blue-600 h-1 w-1 absolute bottom-5 right-0 top-1 ring-4`,
-  navItem: `relative mr-1 cursor-pointer hover:opacity-60`,
-  nav: `flex justify-center items-center gap-[20px]`,
-  header: `bg-[#17171A] text-white h-20 flex gap-[100px] w-full p-[30px]`,
-  headerWrapper: `flex justify-center h-full max-w-screen-xl mx-auto px-4`,
-  inputContainer: `flex items-center justify-center p-2 rounded bg-[#171924]`,
-  input: `bg-transparent outline-none text-white w-70 ml-3`,
-  cursorPointer: `mr-5 cursor-pointer`,
-}
+	navLink: `text-white flex mx-[10px]`,
+	badge: `rounded-full bg-blue-600 h-1 w-1 absolute bottom-5 right-0 top-1 ring-4`,
+	navItem: `relative mr-1 cursor-pointer hover:opacity-60`,
+	nav: `flex justify-center items-center gap-[20px]`,
+	header: `bg-[#17171A] text-white h-20 flex gap-[100px] w-full p-[30px]`,
+	headerWrapper: `flex justify-center h-full max-w-screen-xl mx-auto px-4`,
+	inputContainer: `flex items-center justify-center p-2 rounded bg-[#171924]`,
+	input: `bg-transparent outline-none text-white w-70 ml-3`,
+	cursorPointer: `mr-5 cursor-pointer`,
+};
 
 const Header = () => {
-  return (
-    <div className={styles.header}>
-        <Image
-        src="https://s2.coinmarketcap.com/static/cloud/img/coinmarketcap_white_1.svg"
-        alt='logo'
-        width={220}
-        height={220}
-        />
-    </div>
-  )
-}
+	return (
+		<div className={styles.header}>
+			<Image
+				src="https://s2.coinmarketcap.com/static/cloud/img/coinmarketcap_white_1.svg"
+				alt="logo"
+				width={220}
+				height={220}
+			/>
+			<div className={styles.headerWrapper}>
+				<div className={styles.nav}>
+					<div className={styles.navItem}>
+						<div className={styles.navLink}>
+							Cryptocurrencies
+							<div className={styles.badge} />
+						</div>
+					</div>
+					<div className={styles.navItem}>
+						<div className={styles.navLink}>Exchanges</div>
+					</div>
+					<div className={styles.navItem}>
+						<div className={styles.navLink}>
+							NFTs
+							<div className={styles.badge} />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default Header
+export default Header;
