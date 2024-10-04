2971dad88a6423b52e9b6baf4a3bf0abc36c56ce
hollali <dheztinykartel@gmail.com>
2024-10-04 09:04:30 +0000

    "Added Header component and modified page layout"

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+//import type { NextPage } from "next"
+import Header from '../components/header'
 
 export default function Home() {
   return (
-    <div>
-      Hello
+    <div className="min-h-screen">
+      <Header />
+      <div className="mt-10"/>
+      {/*<Trending />*/}
+      <div className="mt-20"/>
+      {/*<CMC table />*/}   
     </div>
   )
 }
