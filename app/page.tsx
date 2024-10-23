e1b429c1f0669243fca332a61071c90edb7bccc3
hollali <dheztinykartel@gmail.com>
2024-10-23 14:34:17 +0000

    "Updated import statements and added GunProvider and CoinMarketProvider to MyApp component"

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 //import type { NextPage } from "next"
-import Header from '../components/header'
-import Trending from '../components/trending'
+import Header from "../components/header";
+import Trending from "../components/trending";
 export default function Home() {
-  return (
-    <div className="min-h-screen">
-      <Header />
-      <div className="mt-10"/>
-      <Trending />
-      <div className="mt-20"/>
-      {/*<CMC table />*/}   
-    </div>
-  )
+	return (
+		<div className="min-h-screen">
+			<Header />
+			<div className="mt-10" />
+			<Trending />
+			<div className="mt-20" />
+			{/*<CMC table />*/}
+		</div>
+	);
 }
--- pages/app.js
+++ pages/app.js
@@ -1,14 +1,18 @@
-import '../styles/globals.css'
-import { MoralisProvider } from '@moralisweb3/next'
+import "../styles/globals.css";
+import { MoralisProvider } from "@moralisweb3/next";
+import { GunProvider } from "../context/gunContext";
+import { CoinMarketProvider } from "../context/context";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <MoralisProvider
-      apiKey={process.env.NEXT_PUBLIC_MORALIS_API_KEY}
-    >
-      <Component {...pageProps} />
-    </MoralisProvider>
-  )
+	return (
+		<MoralisProvider apiKey={process.env.NEXT_PUBLIC_MORALIS_API_KEY}>
+			<GunProvider>
+				<CoinMarketProvider>
+					<Component {...pageProps} />
+				</CoinMarketProvider>
+			</GunProvider>
+		</MoralisProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;
