c54ba93ecee3c6da72b224bc884d6e027978f27a
hollali <dheztinykartel@gmail.com>
2024-10-23 14:11:17 +0000

    "Updated package-lock.json, package.json, and pages/app.js with new dependencies and configurations"

--- pages/app.js
+++ pages/app.js
@@ -1,14 +1,14 @@
 import '../styles/globals.css'
-import { MoralisProvider } from 'react-moralis'
+import { MoralisProvider } from '@moralisweb3/next'
 
-function MyApp({ Component, pageProps}) {
-	return(
-	<MoralisProvider
-	apiKey='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6ImI3ZjZhODg0LWFhNmUtNDE2NS04NDMzLWUxZGQ2ZjZkZGY3MCIsIm9yZ0lkIjoiNDEwNDIxIiwidXNlcklkIjoiNDIxNzcxIiwidHlwZUlkIjoiM2ViZDZjOTktZmY5My00Y2FiLThjNzUtOTkxM2IwMGNmMzMyIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MjgwMjg2MzAsImV4cCI6NDg4Mzc4ODYzMH0.A5X4q7IQwuWBGlILQcS9XzUG2fQ20OCMkleUqPMyP-8'
-	>
-		<Component {...pageProps} />
-	</MoralisProvider>
-	)
+function MyApp({ Component, pageProps }) {
+  return (
+    <MoralisProvider
+      apiKey={process.env.NEXT_PUBLIC_MORALIS_API_KEY}
+    >
+      <Component {...pageProps} />
+    </MoralisProvider>
+  )
 }
 
 export default MyApp
