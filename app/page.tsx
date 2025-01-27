aa5ef753c5d2c0c1451e89a1e8f35d10ec5d3662
hollali <dheztinykartel@gmail.com>
2025-01-27 12:35:36 +0000

    `Removed unused import and commented out CMCTable component`

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Header from "../components/header";
 import Trending from "../components/trending";
-import CMCTable from'../components/cmc-table/cmcTable'
+//import CMCTable from'../components/cmc-table/cmcTable'
 export default function Home() {
 	return (
 		<div className="min-h-screen">
@@ -8,7 +8,7 @@ export default function Home() {
 			<div className="mt-10" />
 			<Trending />
 			<div className="mt-20" />
-			<CMCTable />
+			{/*<CMCTable />*/}
 		</div>
 	);
 }
